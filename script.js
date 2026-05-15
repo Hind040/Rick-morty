02f16783268b8b3899e4dae7f5f3eb585518bc16
Hind_bencheikh <hind.bc05@gmail.com>
2026-05-15 22:45:13 +0100

    just cretae the index of the page (html code)

---
+++ script.js
@@ -0,0 +1,6 @@
+let currentpage=1
+const ccontainer = document.getElementById("cards-container")
+const prev = document.getElementById("previous")
+const nx = document.getElementById("next")
+const inf = document.getElementById("info")
+
