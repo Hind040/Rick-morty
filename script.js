7efabaa81e46284a555b00edea962cb963c82b54
Hind_bencheikh <hind.bc05@gmail.com>
2026-05-16 01:17:09 +0100

    connect the api and get the characters in card with their informations and thats all with a page numbering

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ const prev = document.getElementById("previous")
 const nx = document.getElementById("next")
 const inf = document.getElementById("info")
 
+async function fetchcharacters(page) {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+    const data = await response.json()
+    return data
+}
+
+async function showcharachters(page) {
+    const data = await fetchcharacters(page)
+    const characters = data.results
+
+
+inf.innerText = `page ${currentpage} / ${data.info.pages}`
+ccontainer.innerHTML=""
+
+characters.forEach(char=>{
+    const card=`
+        <div class="card-header">
+            <img src="${char.image}" alt="${char.name}">
+        <div class="card-body">
+            <p>${char.name}</p>
+            <p>${char.status} - ${char.type}</p>
+            <p>${char.species}</p>
+            <p>${char.origin.name}</p>
+        </div>
+        </div>
+
+    `
+    ccontainer.innerHTML+=card
+})
+}
+
+prev.addEventListener('click',function(){
+   if (currentpage >1){
+    currentpage--
+    showcharachters(currentpage)
+   }
+})
+
+nx.addEventListener('click',function(){
+    currentpage++
+    showcharachters(currentpage)
+})
+showcharachters(currentpage)
